58f454092b4724fe1fe1518d56f3d30edde73fa7
Cristian Pena <cmorgoth@cristians-mbp.guest-network.net>
2016-05-27 17:44:22 -0500

    update

--- HggRazor/LimitTools/app/PlotLimits.cc
+++ HggRazor/LimitTools/app/PlotLimits.cc
@@ -8,6 +8,8 @@
 #include <TTree.h>
 #include <TGraph.h>
 #include <TAxis.h>
+#include <TCanvas.h>
+#include <TStyle.h>
 //LOCAL INCLUDES
 #include "CommandLineInput.hh"
 
@@ -20,6 +22,32 @@ struct Limit
   double exp0p84;
   double exp0p975;
 };
+
+
+const float lumi = 5;
+//Axis
+const float axisTitleSize = 0.06;
+const float axisTitleOffset = .8;
+
+const float axisTitleSizeRatioX   = 0.18;
+const float axisLabelSizeRatioX   = 0.12;
+const float axisTitleOffsetRatioX = 0.94;
+
+const float axisTitleSizeRatioY   = 0.15;
+const float axisLabelSizeRatioY   = 0.108;
+const float axisTitleOffsetRatioY = 0.32;
+
+//Margins
+const float leftMargin   = 0.12;
+const float rightMargin  = 0.05;
+const float topMargin    = 0.07;
+const float bottomMargin = 0.12;
+
+//CMS STANDARD
+TString CMSText = "CMS";
+TString extraText   = "Preliminary";
+//TString lumiText = "2.32 fb^{-1} (13 TeV)";
+TString lumiText = "27 pb^{-1} (13 TeV)";
 
 int main( int argc, char** argv )
 {
@@ -118,6 +146,38 @@ int main( int argc, char** argv )
   TGraph* gOneS = new TGraph(2*npoints, xp, OneS);
   TGraph* gTwoS = new TGraph(2*npoints, xp, TwoS);
 
+  TCanvas* c = new TCanvas( "c", "c", 2119, 33, 800, 700 );
+  c->SetHighLightColor(2);
+  c->SetFillColor(0);
+  c->SetBorderMode(0);
+  c->SetBorderSize(2);
+  c->SetLeftMargin( leftMargin );
+  c->SetRightMargin( rightMargin );
+  c->SetTopMargin( topMargin );
+  c->SetBottomMargin( bottomMargin );
+  c->SetFrameBorderMode(0);
+  c->SetFrameBorderMode(0);
+
+  gStyle->SetPaintTextFormat("4.3f");
+  
+  gTwoS->SetFillColor(kSpring-3);
+  gOneS->SetFillColor(kSpring+10);
+  gExp->SetLineWidth(3);
+  gExp->SetLineStyle(2);
+  gObs->SetLineWidth(3);
+
+  gTwoS->SetTitle("");
+  gTwoS->GetXaxis()->SetTitle("M_{G} (GeV)");
+  gTwoS->GetYaxis()->CenterTitle(kTRUE);
+  gTwoS->GetYaxis()->SetTitle("95% C.L. limit #sigma(pp#rightarrowG#rightarrow#gamma#gamma) (fb)");
+  gTwoS->Draw("AFC");
+  gOneS->Draw("FC");
+  gExp->Draw("C");
+  gObs->Draw("C");
+
+  c->SaveAs("NarrowResLimit.pdf");
+  c->SaveAs("NarrowResLimit.C");
+  
   gObs->GetXaxis()->SetRangeUser(0, 30);
   gObs->Write("gObs");
   gExp->Write("gExp");
