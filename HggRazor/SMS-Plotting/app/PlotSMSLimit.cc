029ceff7b6f5807e5b1dcbb9b7b6cb7dba261d64
Cristian Pena <cmorgoth@cristians-mbp.guest-network.net>
2016-07-16 16:18:17 -0700

    reading input list

--- HggRazor/SMS-Plotting/app/PlotSMSLimit.cc
+++ HggRazor/SMS-Plotting/app/PlotSMSLimit.cc
@@ -26,6 +26,23 @@ int main( int argc, char* argv[] )
     std::cerr << "[ERROR]: please provide an xsec file using --xsecFile=<path_to_file>" << std::endl;
     return -1;
   }
+
+  std::ifstream ifs( inputList.c_str(), std::fstream::in );
+  if ( ifs.is_open() )
+    {
+      std::string fname;
+      while ( ifs.good() )
+	{
+	  ifs >> fname;
+	  if ( ifs.eof() ) break;
+	  std::cout << "[INFO]: fname-> " << fname << std::endl;
+	}
+    }
+  else
+    {
+      std::cout << "Unable to open binning lookup table" << std::endl;
+    }
+  
   
   return 0;
 }
